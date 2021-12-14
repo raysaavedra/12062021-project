6ce7194bb5c18d0707f14d2e5c9be1a0eb0d4456
Ray Saavedra <rayyacosaavedra@gmail.com>
2021-12-14 22:32:26 +0800

    fix

--- frontend/web/src/Components/Property/Detail/Ticker/index.tsx
+++ frontend/web/src/Components/Property/Detail/Ticker/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Box, Grid } from "@mui/material";
+import { Box } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 import { Stat } from "./Stat";
